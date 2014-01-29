a4ed3562155344e400a7661e3b7d883165374e08
upsy-daisy <upsy-daisy@upsydaisy.(none)>
2014-01-29 18:31:20 +0100

    guest mode switch allows to set bluetooth on and off

--- js/handleSetting.js
+++ js/handleSetting.js
@@ -1,5 +1,6 @@
 var foxPrivacyApp = foxPrivacyApp || {};
 var ListOfApps = new Array();
+'use strict';
 (function() {
     /* 
      * Define actions here according to the action string that is passed as actionName and a value.
@@ -26,21 +27,41 @@ var ListOfApps = new Array();
  		console.log('LIST OF APPS:        '+ListOfApps);
  	}
  	if(actionName == 'guestmode-toggle'){
-		console.log('In guest mode');
+ 		var settings = window.navigator.mozSettings;
+ 		if (!settings) {
+ 			console.log('no settings');
+   			return;
+ 		}
+ 		var reqEnabled = settings.createLock().get('bluetooth.enabled');
+    	reqEnabled.onsuccess = function() {
+    		  var isBluetoothEnabled = reqEnabled.result['bluetooth.enabled'];
+      		  console.log('AAAAAAAAA: '+isBluetoothEnabled);
+   		 };
+   		 
+   		var req = settings.createLock().set({'bluetooth.enabled': value});
+    	req.onsuccess = function() {
+    		  var isBluetoothEnabled = reqEnabled.result['bluetooth.enabled'];
+      		  console.log('SET: '+isBluetoothEnabled);
+   		 };
+ 	
+ 	
+	/*	console.log('In guest mode');
 		var settings = window.navigator.mozSettings;
-		console.log('window.navigator.mozSettings');
-		var req = settings.createLock().get('bluetooth.enabled');
-		/*console.log('Lock Works');*/
+		console.log('window navigator mozSettings');
+		
+		var lock = settings.createLock();
+		console.log('settings createLock');
+		
+		var req = lock.set({'wifi.enabled': true});
+		console.log('Set Works');
+		
 		req.onsuccess = function bt_EnabledSuccess() {
       		console.log('BT on');
     	};
    	 	req.onerror = function bt_EnabledOnerror() {
      		console.log('BT off');
-   		 };
+   		 };*/
 		console.log(actionName+'FUNCTION WORKS:'+value);
-  		if (!settings) {
-   		return;
- 		}
  	}
 	
 	if(typeof callback === 'function') {
