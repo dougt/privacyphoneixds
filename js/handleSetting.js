0906a60772d4747f728ade05bf050a580330c248
Joan Perals Tresserra <j.pertres@gmail.com>
2014-01-31 14:55:57 +0100

    Variable rename again

--- js/handleSetting.js
+++ js/handleSetting.js
@@ -1,4 +1,4 @@
-var foxPrivacyApp = foxPrivacyApp || {};
+var app = app || {};
 var ListOfApps = new Array();
 'use strict';
 (function() {
@@ -6,7 +6,7 @@ var ListOfApps = new Array();
      * Define actions here according to the action string that is passed as actionName and a value.
      * "Target" is only used for "secondary" toggles, and will be undefined for "primary" toggles.
      * */
-    foxPrivacyApp.handleSetting = function(actionName, value, target, callback) {
+    app.handleSetting = function(actionName, value, target, callback) {
 
         console.log('actionName: ' + actionName);
 	console.log('value : ' + value);
