1f862dcbe7e8d446d64cddbec4901226964e3785
Joan Perals Tresserra <j.pertres@gmail.com>
2014-02-03 15:02:51 +0100

    Improved permissions history view

--- js/permissionsHistory.js
+++ js/permissionsHistory.js
@@ -3,13 +3,18 @@ var app = app || {};
 (function() {
     var currentTab = 0;
     app.permissionsHistory = {
-        toggleTab : function(elem) {
+        toggleTab : function(elem, index) {
             var rootElem = app.settings.findAncestorWithClass(elem, 'tabbed');
             if(rootElem) {
                 var elements = rootElem.querySelectorAll('.tab');
                 for(var i in elements) {
                     var element = elements.item(i);
-                    element.classList.toggle('selected');
+                    if(element.classList.contains('tab-' + index)) {
+                        element.classList.add('selected');
+                    }
+                    else {
+                        element.classList.remove('selected');
+                    }
                 }
             }
         }
